04ccaae6254f928d1449b15199a489d92339c941
danielsafari143 <danielsafari143@gmail.com>
2023-05-17 13:00:00 +0200

    consume the leaderboard api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './index.css';
+import getScore from './modules/showScore.js';
+import update from './modules/refresh.js';
+import addScore from './modules/addScore.js';
+
+document.getElementById('refresh').addEventListener('click', () => {
+  update(getScore('WCFRPqcmVfA3nAltLwE7'));
+});
+
+document.getElementById('button').addEventListener('click', async () => {
+  const user = document.getElementById('user').value;
+  const score = document.getElementById('score').value;
+  const values = (await addScore('WCFRPqcmVfA3nAltLwE7', user, score));
+  document.getElementById('check').innerHTML = values.result;
+});
---
+++ src/modules/createGame.js
@@ -0,0 +1,12 @@
+async function create() {
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games',
+    {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: 'real game',
+      }),
+    }).then((responce) => responce);
+}
+
+export default create;
